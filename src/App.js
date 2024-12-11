9e1f430c3a8c7659117866de0df669843e7ef12b
Nombuso <nombusomelinda@gmail.com>
2024-12-11 19:00:35 +0200

    React weather app Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Johannesburg" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Nombuso28"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gugulethu Amanda Ndlovu{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/guguamanda/react-weather-app-shecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
